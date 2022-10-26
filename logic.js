b3c1e1f7a11d85fb142c5026c40e7aa07cf1278a
CoderCowMoo <broskichad38@gmail.com>
2022-10-26 20:02:24 +1100

    Add everything?

--- logic.js
+++ logic.js
@@ -1,6 +1,13 @@
+// return random value between 0 and 255
+function randomColour() {
+    return Math.floor(Math.random() * 256);
+}
+
 // DOMOnLoad
 window.onload = function () {
     // get the colourDisplay div
     var colourDisplay = document.getElementById("colourDisplay");
     // give it a random colour
-    colourDisplay.style.backgroundColor = "{}"
+    colourDisplay.style.backgroundColor = `rgb(${randomColour()}, ${randomColour()}, ${randomColour()})`;
+
+    // get the 
