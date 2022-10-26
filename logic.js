5957cd6ba56838ce71847b8bc1ce0b78d16c8712
CoderCowMoo <broskichad38@gmail.com>
2022-10-26 20:15:53 +1100

    Added random colours and buttons with one correct

--- logic.js
+++ logic.js
@@ -1,6 +1,6 @@
 // return random value between 0 and 255
 function randomColour() {
-    return Math.floor(Math.random() * 256);
+    return `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
 }
 
 // DOMOnLoad
@@ -8,6 +8,23 @@ window.onload = function () {
     // get the colourDisplay div
     var colourDisplay = document.getElementById("colourDisplay");
     // give it a random colour
-    colourDisplay.style.backgroundColor = `rgb(${randomColour()}, ${randomColour()}, ${randomColour()})`;
+    let correct_colour = randomColour();
+    colourDisplay.style.backgroundColor = correct_colour;
 
-    // get the 
+    // loop over colourButton class buttons
+    var colourButtons = document.getElementsByClassName("colourButton");
+    // random number between 0 and colourButtons.length
+    let randomIndex = Math.floor(Math.random() * colourButtons.length);
+    for (let i = 0; i < colourButtons.length; i++) {
+        // set innertext of button to random colour
+        if (i != randomIndex) {
+            colourButtons[i].innerText = randomColour();
+        } else {
+            colourButtons[i].innerText = correct_colour;
+        }
+        // add an event listener to each button
+        colourButtons[i].addEventListener("click", function () {
+            guessed(colourButtons[i].innerText);
+        });
+    }
+}
