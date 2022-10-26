49fd2c7c789ce099d7022734ef0167fb5dc4dbb7
CoderCowMoo <broskichad38@gmail.com>
2022-10-26 19:57:45 +1100

    Added barebones and CSS

---
+++ logic.js
@@ -0,0 +1,6 @@
+// DOMOnLoad
+window.onload = function () {
+    // get the colourDisplay div
+    var colourDisplay = document.getElementById("colourDisplay");
+    // give it a random colour
+    colourDisplay.style.backgroundColor = "{}"
